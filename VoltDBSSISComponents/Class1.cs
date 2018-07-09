2f32e2f5efa6f4757828d0e509db951245f45779
seeta somagani <ssomagani@voltdb.com>
2018-07-08 22:48:25 -0400

    Generified the destination component

--- VoltDBSSISComponents/Class1.cs
+++ VoltDBSSISComponents/Class1.cs
@@ -2,9 +2,11 @@
 using Microsoft.SqlServer.Dts.Pipeline.Wrapper;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VoltDB.Data.Client;
 
 namespace VoltDBSSISComponents
 {
@@ -14,6 +16,8 @@ namespace VoltDBSSISComponents
         )]
     public class VoltDBSSISDestination : PipelineComponent
     {
+        VoltConnection connection;
+
         // --------------DESIGN METHODS------------------------//
         public override void ProvideComponentProperties()
         {
@@ -28,19 +32,27 @@ namespace VoltDBSSISComponents
             output.Name = "Output";
             output.SynchronousInputID = input.ID;
 
-            IDTSCustomProperty100 property = ComponentMetaData.CustomPropertyCollection.New();
-            property.Name = "ServerAddress";
-            property.Description = "Hostname/IP address of VoltDB server";
+            IDTSCustomProperty100 serverProp = ComponentMetaData.CustomPropertyCollection.New();
+            serverProp.Name = "ServerAddress";
+            serverProp.Description = "Hostname/IP address of VoltDB server";
+
+            IDTSCustomProperty100 tableProp = ComponentMetaData.CustomPropertyCollection.New();
+            tableProp.Name = "TableName";
+            tableProp.Description = "Name of the VoltDB table to insert into";
 
             ComponentMetaData.ContactInfo = "ssomagani@voltdb.com";
-            ComponentMetaData.Version = 1017;
+            ComponentMetaData.Version = 121506;
         }
 
         // -------------------RUNTIME METHODS ------------------------//
         public override void AcquireConnections(object transaction)
         {
             base.AcquireConnections(transaction);
-            Console.WriteLine("Acquiring connections");
+
+            Debug.WriteLine(ComponentMetaData.CustomPropertyCollection[0]);
+
+            var hostname = ComponentMetaData.CustomPropertyCollection["ServerAddress"].Value;
+            connection = VoltConnection.Create("hosts=" + hostname).Open();
         }
 
         public override void PerformUpgrade(int pipelineVersion)
@@ -50,8 +62,61 @@ namespace VoltDBSSISComponents
 
         public override void ProcessInput(int inputID, PipelineBuffer buffer)
         {
-            base.ProcessInput(inputID, buffer);
-            Console.WriteLine("Processing input " + inputID);
+            var tableName = ComponentMetaData.CustomPropertyCollection["TableName"].Value;
+            IDTSInput100 input = ComponentMetaData.InputCollection.GetObjectByID(inputID);
+            
+            while(buffer.NextRow())
+            {
+                List<String> sqlStmtAruments = new List<string>();
+
+                foreach(IDTSInputColumn100 col in input.InputColumnCollection)
+                {
+                    int colIndex = BufferManager.FindColumnByLineageID(input.Buffer, col.LineageID);
+                    sqlStmtAruments.Add(buffer[colIndex].ToString());
+                }
+
+                String sqlStmt = "insert into " + tableName + " values (" + String.Join(", ", sqlStmtAruments) + ")";
+
+                var Insert = connection.Procedures.Wrap<Null, string>("@AdHoc");
+
+                Insert.Execute(sqlStmt);
+
+                /* 
+
+                 var genericMethod = typeof(ProcedureAccess).GetMethod("Wrap");
+                 var typedMethod = genericMethod.MakeGenericMethod(
+                     typeof(Null), 
+                     typeof(Int32),
+                     typeof(Double),
+                     typeof(Double),
+                     typeof(Double),
+                     typeof(Double),
+                     typeof(Double),
+                     typeof(Int32)
+                     );
+
+                 object[] procNameParam = { "InsertOrder" };
+
+                 var Insert = typedMethod.Invoke(connection.Procedures, procNameParam);
+                 //var Insert = connection.Procedures.Wrap<Null, Int32, Double, Double, Double, Double, Double, Int32>("InsertOrder");
+
+                 var genericProcMethod = typeof(ProcedureWrapper).GetMethod("Execute");
+                 var typedProcMethod = genericProcMethod.MakeGenericMethod(
+                     typeof(Null),
+                     typeof(Int32),
+                     typeof(Double),
+                     typeof(Double),
+                     typeof(Double),
+                     typeof(Double),
+                     typeof(Double),
+                     typeof(Int32)
+                     );
+
+                 object[] procArgs = { productCode, shippingWeight, shippingLength, shippingWidth, shippingHeight, unitCost, perOrder };
+                 typedProcMethod.Invoke(Insert, procArgs);*/
+
+                //Insert.Execute(productCode, shippingWeight, shippingLength, shippingWidth, shippingHeight, unitCost, perOrder);
+            }
         }
     }
 }
